e276b9ff9d43760e9e79b3f41b36f120850b0992
negima0-0 <51804406+negima0-0@users.noreply.github.com>
2024-02-27 23:14:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import datetime
 import chardet
 import re
 
-
+# textから読む形式になっている
+# csvとかでやれたほうが対象が多いときに楽そうだからcsvから対象を読み込むよう改修する
 def detect_encoding(filename):
     with open(filename, "rb") as f:
         result = chardet.detect(f.read())
@@ -31,12 +32,12 @@ def login_and_execute_commands(host_name, ip_address, username, password, comman
         command_file = re.sub(r'\|.*$', '', command)
         print("retrieving " + str(command_file) + "data. please wait...")
         output = connection.send_command(command, read_timeout=300)
-        # outputs.append(output)
+        # outputs.append(output) #出力をすべてマージするときはこのコメントアウトを外していい感じにする
         timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
         command_file = re.sub(r'\|.*$', '', command)
         logfile = f"{host_name}_{ip_address}_{timestamp}_{command_file}.txt"
         with open(logfile, 'w') as f:
-            # for output in outputs:
+            # for output in outputs: #出力をすべてマージするときはこのコメントアウトを外していい感じにする
             f.write(output + "\n\n")
 
 
